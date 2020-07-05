df9772b90c29fb646a6b9eb19ddba77d3bff2f0d
veedahv <veedahv55@gmail.com>
2020-07-05 22:57:20 +0100

    data binding error

---
+++ app/src/main/java/com/example/books/BookListActivity.java
@@ -0,0 +1,128 @@
+package com.example.books;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuItemCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class BookListActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+    private ProgressBar mLoaingProgress;
+    private RecyclerView rvBooks;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_list);
+        mLoaingProgress = (ProgressBar) findViewById(R.id.pb_loading);
+        rvBooks = (RecyclerView) findViewById(R.id.rv_books);
+        LinearLayoutManager booksLayoutManager = new LinearLayoutManager(this,
+                LinearLayoutManager.VERTICAL, false);
+        rvBooks.setLayoutManager(booksLayoutManager);
+        try {
+            URL bookUrl = ApiUtil.buildUrl("cooking");
+            new BooksQueryTask( ).execute(bookUrl);
+
+        } catch (Exception e) {
+            Log.d("error", e.getMessage());
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.book_list_menu, menu);
+        final MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(this);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_advanced_search:
+                Intent intent = new Intent(this, SearchActivity.class);
+                startActivity(intent);
+                return true;
+
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+
+        try {
+            URL bookUrl = ApiUtil.buildUrl(query);
+            new BooksQueryTask().execute(bookUrl);
+        }
+        catch (Exception e) {
+            Log.d("error", e.getMessage());
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
+    }
+
+    public class BooksQueryTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL searchUrl = urls[0];
+            String result = null;
+            try {
+                result = ApiUtil.getJson(searchUrl);
+            } catch (IOException e) {
+                Log.e("Error", e.getMessage());
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            TextView tvError = (TextView) findViewById(R.id.tv_error);
+            mLoaingProgress.setVisibility(View.INVISIBLE);
+            if (result == null) {
+                rvBooks.setVisibility(View.INVISIBLE);
+                tvError.setVisibility(View.VISIBLE);
+           }
+            else {
+                rvBooks.setVisibility(View.VISIBLE);
+                tvError.setVisibility(View.INVISIBLE);
+            }
+            ArrayList<Book> books = ApiUtil.getBooksFromJson(result);
+            String resultString = "";
+            BooksAdapter adapter = new BooksAdapter(books);
+            rvBooks.setAdapter(adapter);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoaingProgress.setVisibility(View.VISIBLE);
+        }
+    }
+}
